d7a104b02c56948b375f72d18fe579519476a05a
majemase <majemase12@gmail.com>
2024-12-15 14:07:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/granja/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.granja;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    MaterialToolbar menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
